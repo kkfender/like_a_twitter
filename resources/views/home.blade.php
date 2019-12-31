22688bd17e2af3352997c9c60cc05c74d3d8d2af
vagrant <vagrant@homestead>
2019-12-31 15:41:10 +0000

    ごみをきれいに・・・

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,8 +1,23 @@
 @extends('layouts.app')
-@section('content')
-<p>これから機能を追加していきます</p>
-<a href="/login">ログイン</a>
-<a href="/login">ログイン</a>
 
-<a href="/task">a</a>
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Dashboard</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    You are logged in!
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,14 @@ use App\Task;
 use Illuminate\Http\Request;
 
 //Home
-Route::get('/','HomeController@index');
 Route::get('/login','HomeController@login');
 
 //tasks
-//Route::get('/','TaskController@index');
-Route::post('/task', 'TaskController@create');
-Route::delete('/task/{task}','TaskController@delete');
 
-//users
 //Route::
+Auth::routes();
+Route::get('/', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
