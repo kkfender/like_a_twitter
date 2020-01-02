726c3826e23553ce93dbcd1fbba93754c807c412
vagrant <vagrant@homestead>
2020-01-02 03:43:05 +0000

    routes chenged

--- routes/web.php
+++ routes/web.php
@@ -9,10 +9,8 @@ Route::get('/login','HomeController@login');
 
 //Route::
 Auth::routes();
-<<<<<<< HEAD
+
 Route::get('', 'HomeController@index')->name('home');
-=======
-Route::get('/', 'HomeController@index')->name('home');
->>>>>>> #7
+
 
 Route::post('/post', 'PostController@post');
