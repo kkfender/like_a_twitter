0f0fa79b93e5d24222b0ad2f743a722709b77a92
vagrant <vagrant@homestead>
2020-01-02 05:59:57 +0000

    carbon

--- app/Model/User.php
+++ app/Model/User.php
@@ -7,6 +7,7 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use App\Post;
 use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
 
 
 class User extends Authenticatable
@@ -51,7 +52,47 @@ class User extends Authenticatable
                         ->orderBy('posts.created_at', 'desc')
                         ->get();
 
+        $now = Carbon::now();
 
+    foreach ($postData as $postDatum)
+    {
+        $postTime = new Carbon($postDatum->created_at);
+
+        if(!$postTime->isSameYear($now))
+        {
+            //日付を返す
+            $postDatum->created_at = $postTime->format('Y年m月d日');
+        }
+        else if(!$postTime->isSameMonth($now))
+        {
+            //日付を返す
+            $postDatum->created_at = $postTime->format('Y年m月d日');
+        }
+        else if(!$postTime->isSameDay($now))
+        {
+            //日付を返す
+            $postDatum->created_at = $postTime->format('Y年m月d日');
+        }
+        else if(!$postTime->isSameHour($now))
+        {
+            //何時間前かを返す
+            $postDatum->created_at = $postTime->diffInHours($now).'時間前';
+        }
+        else if(!$postTime->isSameMinute($now))
+        {
+            //何分前かを返す
+            $postDatum->created_at = $postTime->diffInMinutes($now).'分前';
+        }
+        else if(!$postTime->isSameSecond($now))
+        {
+            //何秒前
+            $postDatum->created_at = $postTime->diffInSeconds($now).'秒前';
+        }
+        else
+        {
+            $postDatum->created_at= 'now!';
+        }
+    }
          return $postData;
     }
 }
