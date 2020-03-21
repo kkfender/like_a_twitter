e18ae8f56ce70fc37c9357c229f770f4c258c066
kkfender <[ibanez19851030@yahoo.co.jp]>
2020-03-21 15:20:02 +0000

    profilesモデルとmigrationの作成

---
+++ database/migrations/2020_03_22_000029_create_profiles_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProfilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profiles', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('userName');
+            $table->string('accountName');
+            $table->text('introduction')->nullable();
+            $table->date('birthday')->nullable();
+            $table->string('avatar_filename')->nullable();
+            $table->string('background_filename')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('profiles');
+    }
+}
